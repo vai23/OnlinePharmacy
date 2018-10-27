faa192c167764c0fd17e9855f5eecbb3d82e5f46
Vaibhav Mishra <vaibhavmishra740@gmail.com>
2018-10-26 21:01:23 -0800

    Corrected EditText Flaw

--- app/src/main/java/com/example/vaibhavmishra/onlinepharmacy/SignupPage.java
+++ app/src/main/java/com/example/vaibhavmishra/onlinepharmacy/SignupPage.java
@@ -1,7 +1,9 @@
 package com.example.vaibhavmishra.onlinepharmacy;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Toolbar;
 
 public class SignupPage extends AppCompatActivity {
@@ -10,5 +12,10 @@ public class SignupPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup_page);
+    }
+
+    public void back(View view) {
+        Intent intent=new Intent(this,MainActivity.class);
+        finish();
     }
 }
