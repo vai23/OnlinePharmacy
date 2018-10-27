721e4ec78fdeb140ef10361fb18d95c6e5455319
Vaibhav Mishra <vaibhavmishra740@gmail.com>
2018-10-27 13:17:55 -0800

    Options Menu added and changed some constraints on login page

--- app/src/main/java/com/example/vaibhavmishra/onlinepharmacy/OnlinePharmacy.java
+++ app/src/main/java/com/example/vaibhavmishra/onlinepharmacy/OnlinePharmacy.java
@@ -2,6 +2,8 @@ package com.example.vaibhavmishra.onlinepharmacy;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class OnlinePharmacy extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class OnlinePharmacy extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_online_pharmacy);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu m)
+    {
+        MenuInflater inflater=getMenuInflater();
+        inflater.inflate(R.menu.pharmacy_menu,m);
+        return true;
     }
 }
