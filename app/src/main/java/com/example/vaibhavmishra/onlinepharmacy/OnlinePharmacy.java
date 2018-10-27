86b43d08ba83d9dbb9ccb4ae2bd97bf644b406b9
Vaibhav Mishra <vaibhavmishra740@gmail.com>
2018-10-27 14:05:11 -0800

    Modified OnlinePharmacy page

--- app/src/main/java/com/example/vaibhavmishra/onlinepharmacy/OnlinePharmacy.java
+++ app/src/main/java/com/example/vaibhavmishra/onlinepharmacy/OnlinePharmacy.java
@@ -1,11 +1,15 @@
 package com.example.vaibhavmishra.onlinepharmacy;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.widget.Toast;
 
 public class OnlinePharmacy extends AppCompatActivity {
+
+    private int countBack=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,4 +24,32 @@ public class OnlinePharmacy extends AppCompatActivity {
         inflater.inflate(R.menu.pharmacy_menu,m);
         return true;
     }
+
+    public void onBackPressed(){
+        if(countBack<1)
+        {
+            Toast toast=Toast.makeText(this,"Press again to exit",Toast.LENGTH_SHORT);
+            toast.show();
+            countBack++;
+            return;
+        }
+        countBack=0;
+        Intent a = new Intent(Intent.ACTION_MAIN);
+        a.addCategory(Intent.CATEGORY_HOME);
+        a.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(a);
+    }
+
+    /*
+     public void onBackPressed() {
+        finish();
+     }
+
+     <activity
+     android:name="com.example.shoppingapp.AddNewItems"
+     android:label=""
+     android:noHistory="true">
+     </activity>
+
+    */
 }
