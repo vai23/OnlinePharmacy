63d0ab56811fe2bb3a56d706ea948370badd21e0
Vaibhav Mishra <vaibhavmishra740@gmail.com>
2018-10-28 01:52:23 -0800

    Added navigation bar

--- app/src/main/java/com/example/vaibhavmishra/onlinepharmacy/OnlinePharmacy.java
+++ app/src/main/java/com/example/vaibhavmishra/onlinepharmacy/OnlinePharmacy.java
@@ -1,20 +1,34 @@
 package com.example.vaibhavmishra.onlinepharmacy;
 
 import android.content.Intent;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.Toast;
+import android.support.v7.widget.Toolbar;
 
 public class OnlinePharmacy extends AppCompatActivity {
 
+    private DrawerLayout drawerLayout;
     private int countBack=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_online_pharmacy);
+        Toolbar toolbar=findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar=getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
+
+        drawerLayout=findViewById(R.id.drawer_layout);
     }
 
     @Override
@@ -23,6 +37,18 @@ public class OnlinePharmacy extends AppCompatActivity {
         MenuInflater inflater=getMenuInflater();
         inflater.inflate(R.menu.pharmacy_menu,m);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        switch (item.getItemId())
+        {
+            case android.R.id.home:
+                drawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     public void onBackPressed(){
