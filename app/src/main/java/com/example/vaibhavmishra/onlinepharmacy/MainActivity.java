99eb4217569ad934ba2ae6ca8d52ce65d339cf57
Vaibhav Mishra <vaibhavmishra740@gmail.com>
2018-10-29 20:55:19 -0800

    Modified check box

--- app/src/main/java/com/example/vaibhavmishra/onlinepharmacy/MainActivity.java
+++ app/src/main/java/com/example/vaibhavmishra/onlinepharmacy/MainActivity.java
@@ -4,13 +4,32 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    private CheckBox owner, staff;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        owner=findViewById(R.id.owner);
+        staff=findViewById(R.id.staff);
+        owner.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener(){
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                staff.setChecked(!owner.isChecked());
+            }
+        });
+        staff.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener(){
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                owner.setChecked(!staff.isChecked());
+            }
+        });
     }
 
     public void signupPressed(View view) {
--- app/src/main/java/com/example/vaibhavmishra/onlinepharmacy/OnlinePharmacy.java
+++ app/src/main/java/com/example/vaibhavmishra/onlinepharmacy/OnlinePharmacy.java
@@ -6,10 +6,14 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
 import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
 import android.widget.Toast;
 import android.support.v7.widget.Toolbar;
 
